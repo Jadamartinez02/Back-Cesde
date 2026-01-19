a1c5482d5b6f15372c47187bc8ba1cb13c3f4a1c
Jadamartinez02 <javier.martinez02@globant.com>
2026-01-19 10:53:05 -0500

    Creacion repositorio

---
+++ src/main/java/com/pangea/pangea/PangeaApplication.java
@@ -0,0 +1,13 @@
+package com.pangea.pangea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PangeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PangeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pangea/pangea/PangeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pangea.pangea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PangeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
